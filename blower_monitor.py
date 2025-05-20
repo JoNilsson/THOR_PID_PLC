d0064ea9c7d18ad4863f3da0b3a82ebd710049b9
gotoXY <gotoxy@teknomadix.com>
2025-05-20 12:27:16 -0700

    refactor, use boilerplate GPIO methods

--- blower_monitor.py
+++ blower_monitor.py
@@ -1,12 +1,12 @@
 """
 Blower Monitor Module for THOR SiC Heater Control System
+Author: Johanness A. Nilsson for TERRAFORM INDUSTRIES 2025
 Monitors blower operation via current sensing SSR on GPIO Pin 14
 """
 
+import time
 import board
 import digitalio
-import time
-from P1AM import Base
 
 class BlowerMonitor:
     """Monitors blower operation via current sensing SSR on GPIO Pin 14"""
@@ -19,17 +19,26 @@ class BlowerMonitor:
             required_states: List of system states where blower must be running
             error_callback: Function to call when blower failure detected
         """
-        # Use GPIO Pin 14 for blower current sensing SSR
-        self.blower_pin = board.GP14
-        self.blower_input = digitalio.DigitalInOut(self.blower_pin)
-        self.blower_input.direction = digitalio.Direction.INPUT
-        self.blower_input.pull = digitalio.Pull.UP  # Pull-up, so LOW means current detected
+        # Standard CircuitPython digitalio for GPIO access
+        # try to access GPIO Pin 14 on the P1AM-GPIO shield
+        # Docs on Py P1AM GPIO are sparese, so using some boilerplate micropython methods
+        try:
+            # D14 = GPIO digital pin 14
+            self.blower_pin = digitalio.DigitalInOut(board.D14)
+            self.blower_pin.direction = digitalio.Direction.INPUT
+            self.blower_pin.pull = digitalio.Pull.UP  # Pull-up, so LOW means current detected
+        except (AttributeError, ValueError) as e:
+            # If D14 isn't available directly, try using a generic pin reference
+            import microcontroller
+            self.blower_pin = digitalio.DigitalInOut(microcontroller.pin.P14)
+            self.blower_pin.direction = digitalio.Direction.INPUT
+            self.blower_pin.pull = digitalio.Pull.UP
 
         self.required_states = required_states or []
         self.error_callback = error_callback
         self.blower_status = False
         self.last_check_time = time.monotonic()
-        self.check_interval = 0.5  # Check every 500ms by default
+        self.check_interval = 0.5  # Check every 500ms
         self.error_code = 101  # Default error code for blower issues
 
     def is_blower_running(self):
@@ -37,9 +46,9 @@ class BlowerMonitor:
         Check if blower is running based on current sensor
         Returns True if running, False if not
         """
-        # LOW signal (False) means current detected (blower running)
-        # Invert the input value since the SSR closes (LOW) when current is detected
-        return not self.blower_input.value
+        # LOW signal (False) means current detected == blower running
+        # Invert value; SSR closes (LOW) when current is detected
+        return not self.blower_pin.value
 
     def check_blower(self, current_state):
         """
@@ -53,7 +62,7 @@ class BlowerMonitor:
         """
         current_time = time.monotonic()
 
-        # Only check at specified intervals to avoid excessive polling
+        # Only check at specified intervals to avoid overload
         if current_time - self.last_check_time < self.check_interval:
             return True, None
 
@@ -64,7 +73,7 @@ class BlowerMonitor:
         if current_state in self.required_states and not self.blower_status:
             if self.error_callback:
                 return False, self.error_callback(
-                    self.error_code,
+                    self.error_code, 
                     "Blower not running - airflow required for operation"
                 )
             return False, None
@@ -83,4 +92,3 @@ class BlowerMonitor:
             return True, "Blower operation verified"
         else:
             return False, "Blower not running - check blower power and connections"
-
